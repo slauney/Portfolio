c59bbd0fcb9a7f62a49b6ee74347fad23da350ac
slauney <43139808+slauney@users.noreply.github.com>
2019-03-27 13:19:42 -0300

    Update MusicTrack.java

--- Java/Assignments/Assignment5/MusicTrack.java
+++ Java/Assignments/Assignment5/MusicTrack.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 //Description!
 //In this program, we will be creating contructors, stubs, and booleans which we will
